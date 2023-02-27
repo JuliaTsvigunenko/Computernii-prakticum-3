fd9fd7d4fdb0e70bea9df789800f123634e52073
JuliaTsvigunenko <juliatsvigunenko5@gmail.com>
2023-02-28 00:18:29 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Компьютерный практикум №3
+
+            // Задача 1
+            //Console.Write("Введите кол-во элементов массива:");
+            //int elementsCount = int.Parse(Console.ReadLine());
+            //int[] omas = new int[elementsCount];
+            //for ( int i=0; i<omas.Length; i++)
+            //{
+            //    Console.Write($"Введите элемент массива под индексом {i}:");
+            //    omas[i] = int.Parse(Console.ReadLine());
+            //}
+            //Console.WriteLine("Вывод массива:");
+            //for ( int i=0; i<omas.Length; i++)
+            //{
+            //    Console.WriteLine(omas[i]);
+            //}
+
+            //Задача 3
+            //int[] omas = { -3, 5, 18, 19, -2, -4, 0, 9 };
+            //int max = omas[0];
+            //int indexMax = 0;
+            //for (int i = 1; i < omas.Length; i++)
+            //{
+            //    if (omas[i] > max)
+            //    {
+            //        max = omas[i];
+            //        indexMax = i;
+            //    }
+            //}
+
+            //int min = omas[0];
+            //int indexMin = 0;
+            //for (int i = 1; i < omas.Length; i++)
+            //{
+            //    if (omas[i] < min)
+            //    {
+            //        min = omas[i];
+            //        indexMin = i;
+            //    }
+            //}
+
+            //Console.WriteLine($"MAX ={max}\tindexMax={indexMax}\tmin={min}\tindex={indexMin}");
+
+            //omas[indexMax] = min;
+            //omas[indexMin] = max;
+            //foreach (int elem in omas)
+            //{
+            //    Console.Write($"{elem}\t");
+            //}
+
+            // Задача 2
+            //int[] omas = new int[16];
+            //Random random = new Random();
+
+            //for (int i = 0; i < 15; i++)
+            //{
+            //    omas[i] = random.Next(1, 15);
+            //}
+            //foreach (int i in omas)
+            //{ 
+            //    Console.WriteLine(omas[i]);
+            //}
+        }
+    }
+}
